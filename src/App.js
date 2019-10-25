04c7558eb14973fc3e063a8209571e50cbdad888
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-10-25 05:51:20 +0000

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import "./App.css";
+
 import Login from "pages/Login";
 import Register from "pages/Register";
 import Dashboard from "pages/Dashboard";
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import "./index.css";
+
+import { store } from "store";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { store } from "store";
 
 ReactDOM.render(
   <Provider store={store}>
