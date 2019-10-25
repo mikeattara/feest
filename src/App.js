4166fe1724ea4a1e512d3f5176d0ad8b85f1c875
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-10-25 07:19:19 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import PrivateRoute from "components/PrivateRoute";
+import Dashboard from "pages/Dashboard";
 import Login from "pages/Login";
 import Register from "pages/Register";
-import Dashboard from "pages/Dashboard";
 
 const App = () => {
   return (
